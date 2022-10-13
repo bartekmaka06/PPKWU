58c9028499a81163dc118fd833bd86d43a1b9058
bartekmaka <228805@edu.p.lodz.pl>
2022-10-13 13:42:44 +0200

    added time

--- lab1/source/server.py
+++ lab1/source/server.py
@@ -2,7 +2,7 @@
 import http.server
 import socketserver
 import os
-
+from datetime import datetime
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -18,6 +18,8 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()            
             self.wfile.write(b"Hello World!\n")
+            now = datetime.now()
+            self.wfile.write(now.strftime("%H:%M:%S").encode("UTF-8"))
         else:
             super().do_GET()
     
