0ca17ed73d778fa19f52886578551dfb6fd4d2b9
emakbar <228805.edu.p.lodz.pl>
2022-11-10 13:09:34 +0100

    added json

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+import json
 from datetime import datetime
 
 #print('source code for "http.server":', http.server.__file__)
@@ -26,7 +27,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             
             tab = self.path.split('/?str=')
             string = tab[1]
-            #self.wfile.write(string.encode("UTF-8")) 
+            
 
             uppercase_count = sum(1 for char in string if char.isupper())
             lowercase_count = sum(1 for char in string if char.islower())
@@ -34,40 +35,13 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             special_count = len(string)-uppercase_count-lowercase_count-digit_count
 
             show = str(uppercase_count) + " " + str(lowercase_count) + " " + str(digit_count) +" "+str(special_count)
+            myList = [{"lowercase": lowercase_count, "uppercase": uppercase_count, "digits":digit_count, "special":special_count}]
+            jsonString = json.dumps(myList, indent=4)
 
-            self.wfile.write(show.encode("UTF-8")) 
+            self.wfile.write(jsonString.encode("UTF-8")) 
 
         else:
             tab = self.path.split('/?str=')
-        # elif self.path == '/?cmd=time':
-        #     self.protocol_version = 'HTTP/1.1'
-        #     self.send_response(200)
-        #     self.send_header("Content-type", "text/html; charset=UTF-8")
-        #     self.end_headers()       
-        #     now = datetime.now()
-        #     self.wfile.write(now.strftime("%H:%M:%S").encode("UTF-8"))     
-        # else:
-           
-        #     params = self.path.split('&')
-           
-        #     first = params[0]
-        #     first = first[2:].split('=')
-        #     second = params[1].split('=')
-          
-        #     first_name=first[0]
-        #     first_val=first[1]
-            
-        #     second_name=second[0]
-        #     second_val=second[1]
-            
-        #     if first_name=='cmd' and first_val=='rev'and second_name=='str':
-        #         self.protocol_version = 'HTTP/1.1'
-        #         self.send_response(200)
-        #         self.send_header("Content-type", "text/html; charset=UTF-8")
-        #         self.end_headers() 
-        #         self.wfile.write(second_val[::-1].encode("UTF-8"))
-        #     else:
-        #         super().do_GET()
     
 # --- main ---
 
