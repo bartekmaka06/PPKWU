59efecc7ca42ab2cf1ae6a2dcc2029dc01170ca0
228805 <228805@edu.p.lodz.pl>
2022-11-24 13:08:52 +0100

    added json

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -20,50 +20,23 @@ class web_server(http.server.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=UTF-8")
         self.end_headers()            
-        url = 'https://www.example.com/some_path?some_key=some_value'
-        #parsed_url = urlparse(url)
         parsed_url = urlparse(self.path)
 
         params = parse_qs(parsed_url.query)
         if len(params)==2:
             num1=params['num1'][0]
             num2=params['num2'][0]
-
-            self.wfile.write(num1.encode("UTF-8"))
-            self.wfile.write(b"\n")
-            self.wfile.write(num2.encode("UTF-8"))
-
-
-        #self.wfile.write(b"Hello World!\n")
-        # if self.path == '/':
-        #     self.protocol_version = 'HTTP/1.1'
-        #     self.send_response(200)
-        #     self.send_header("Content-type", "text/html; charset=UTF-8")
-        #     self.end_headers()            
-        #     self.wfile.write(b"Hello World!\n")
-        # elif self.path.startswith('/?str='):
-        #     self.protocol_version = 'HTTP/1.1'
-        #     self.send_response(200)
-        #     self.send_header("Content-type", "text/html; charset=UTF-8")
-        #     self.end_headers()       
-            
-        #     tab = self.path.split('/?str=')
-        #     string = tab[1]
-            
-
-        #     uppercase_count = sum(1 for char in string if char.isupper())
-        #     lowercase_count = sum(1 for char in string if char.islower())
-        #     digit_count = sum(1 for char in string if char.isdigit())
-        #     special_count = len(string)-uppercase_count-lowercase_count-digit_count
-
-        #     show = str(uppercase_count) + " " + str(lowercase_count) + " " + str(digit_count) +" "+str(special_count)
-        #     myList = [{"lowercase": lowercase_count, "uppercase": uppercase_count, "digits":digit_count, "special":special_count}]
-        #     jsonString = json.dumps(myList, indent=4)
-
-        #     self.wfile.write(jsonString.encode("UTF-8")) 
-
-        # else:
-        #     tab = self.path.split('/?str=')
+            num1=int(num1)
+            num2=int(num2)
+            myList = [{"sum": int(num1+num2),
+                        "sub": int(num1-num2), 
+                        "mul": int(num1*num2), 
+                        "div":int(num1/num2),
+                        "mod":int(num1%num2)}]
+            jsonString = json.dumps(myList, indent=4)
+            self.wfile.write(jsonString.encode("UTF-8")) 
+            print("hello")
+            print(jsonString)
     
 # --- main ---
 
