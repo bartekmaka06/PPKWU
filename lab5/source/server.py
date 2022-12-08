ce395499d5ee996bfecc091699b699f45a90835d
emakbar <228805@edu.p.lodz.pl>
2022-12-08 13:11:50 +0100

    added str stats  counting

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -4,6 +4,17 @@ import re
 
 app = Flask(__name__)
 
+def calculateString(ss: str):
+    uppercase_count = sum(1 for char in ss if char.isupper())
+    lowercase_count = sum(1 for char in ss if char.islower())
+    digit_count = sum(1 for char in ss if char.isdigit())
+    special_count = len(ss)-uppercase_count-lowercase_count-digit_count
+    return{
+        "lowercase": lowercase_count,
+        "uppercase": uppercase_count,
+        "digits": digit_count,
+        "special": special_count
+    }
 
 @app.route("/", methods=['POST'])
 def main():
@@ -12,12 +23,7 @@ def main():
     num1 = request_json.get("num1")
     num2 = request_json.get("num2")
     
-    output={
-        "str":inputStr,
-        "num1":num1,
-        "num2":num2
-
-    }
+    output=calculateString(inputStr)
     return output
 
 
