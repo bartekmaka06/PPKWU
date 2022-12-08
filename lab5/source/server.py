353c9fe0ec459837248259b05647453f63f689c2
emakbar <228805@edu.p.lodz.pl>
2022-12-08 13:03:04 +0100

    switched to flask

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -1,48 +1,24 @@
 #!/usr/bin/env python3
-import http.server
-import socketserver
-import os
-import json
-import requests
-from datetime import datetime
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+from flask import Flask, request, jsonify
+import re
+
+app = Flask(__name__)
 
 
-#print('source code for "http.server":', http.server.__file__)
-
-class web_server(http.server.SimpleHTTPRequestHandler):
+@app.route("/", methods=['POST'])
+def main():
+    request_json = request.get_json()
+    inputStr = request_json.get("str")
+    num1 = request_json.get("num1")
+    num2 = request_json.get("num2")
     
-    def do_GET(self):
+    output={
+        "str":inputStr,
+        "num1":num1,
+        "num2":num2
 
-        print(self.path)
-        o = urlparse(self.path)
-        self.protocol_version = 'HTTP/1.1'
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=UTF-8")
-        self.end_headers()            
-        parsed_url = urlparse(self.path)
+    }
+    return output
 
-        params = parse_qs(parsed_url.query)
-        if len(params)==2:
-            num1=params['num1'][0]
-            num2=params['num2'][0]
-            num1=int(num1)
-            num2=int(num2)
-            myList = [{"sum": int(num1+num2),
-                        "sub": int(num1-num2), 
-                        "mul": int(num1*num2), 
-                        "div":int(num1/num2),
-                        "mod":int(num1%num2)}]
-            jsonString = json.dumps(myList, indent=4)
-            self.wfile.write(jsonString.encode("UTF-8")) 
-            print(jsonString)
-    
-# --- main ---
 
-PORT = 4080
-
-print(f'Starting: http://localhost:{PORT}')
-
-tcp_server = socketserver.TCPServer(("",PORT), web_server)
-tcp_server.serve_forever()
+app.run(port=4080, host='0.0.0.0')
